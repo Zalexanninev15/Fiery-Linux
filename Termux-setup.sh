afc2fa3da2bff5c7e8678c898ba6c82693d12df7
Max <blue.shark@disroot.org>
2024-11-13 02:04:27 +0300

    Update Termux-setup.sh

--- Termux-setup.sh
+++ Termux-setup.sh
@@ -9,23 +9,23 @@ echo "Copyright (C) 2024-2025 Zalexanninev15"
 echo "License: GPLv3"
 sleep 5
 echo "Process of installing the usual person's required minimum packages is underway :)"
-echo 3
+echo 2
 pkg install termux-am nala curl wget git micro openssl libqrencode zbar python3 cmake ranger -y && apt update && apt upgrade -y && echo "Press 'Allow' in the window that appears" && echo "or type 'Y' if you already have access" && sleep 5
-echo "Process is underway to grant rights to the repository"
+echo "Process is underway to grant rights to the storage (Android)"
 termux-setup-storage
 echo "Applying the recommended settings for ranger..."
-sleep 3
+sleep 2
 git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons && echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
 export VISUAL=micro && export PAGER=more 
 echo "Author's personal recommendation is to customize the appearance of Termux 🤩"
 echo "Settings by author: colors - 73, font - 11"
 echo 5
-git clone https://github.com/adi1090x/termux-style ~/storage/downloads/termux-style
+git clone https://github.com/Zalexanninev15/termux-style-clone.git ~/storage/downloads/termux-style
 cd termux-style && bash ./install
 echo "Removing 'garbage' packages..."
-echo 3
+echo 2
 apt autoremove -y && apt autoclean
 echo "Removing 'Hello-message' in Termux"
-echo 3
+echo 2
 touch ~/.hushlogin
 echo "Done!"
