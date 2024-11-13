f0fa7fee198f9ce8180afa9b494aeca456844301
Max <blue.shark@disroot.org>
2024-11-13 10:30:24 +0300

    Update Termux-setup.sh

--- Termux-setup.sh
+++ Termux-setup.sh
@@ -7,17 +7,18 @@ echo "👇👇👇"
 echo "Created by Zalexanninev15"
 echo "Copyright (C) 2024-2025 Zalexanninev15"
 echo "License: GPLv3"
-sleep 4
+sleep 3
 echo "Process of installing the usual person's required minimum packages is underway :)"
 pkg install termux-am nala curl wget git micro openssl libqrencode zbar python3 ranger -y && echo "Press 'Allow' in the window that appears" && echo "or type 'Y' if you already have access" && echo "Process is underway to grant rights to the storage (Android)" && sleep 2
+echo "Wait..."
 termux-setup-storage
-sleep 5
+sleep 12
 echo "Applying the recommended settings for ranger..."
 git clone "https://github.com/alexanderjeurissen/ranger_devicons.git" ~/.config/ranger/plugins/ranger_devicons && echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
 export VISUAL=micro && export PAGER=more 
 echo "Author's personal recommendation is to customize the appearance of Termux 🤩"
 echo "Settings by author: colors - 73, font - 11"
-echo 4
+echo 3
 git clone "https://github.com/Zalexanninev15/termux-style-clone.got" ~/storage/downloads/termux-style
 cd termux-style && bash ./install
 echo "Removing 'garbage' packages..."
