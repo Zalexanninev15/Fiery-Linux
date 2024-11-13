642f067d0db11fc1c0babb81a172184208116d89
Max <blue.shark@disroot.org>
2024-11-13 10:50:31 +0300

    Update Termux-setup.sh

--- Termux-setup.sh
+++ Termux-setup.sh
@@ -15,11 +15,13 @@ termux-setup-storage
 sleep 12
 git config --global http.postBuffer 524288000
 echo "Applying the recommended settings for ranger..."
+rm -rf ~/.config/ranger/plugins/ranger_devicons
 git clone "https://github.com/alexanderjeurissen/ranger_devicons.git" ~/.config/ranger/plugins/ranger_devicons && echo "default_linemode devicons" >> $HOME/.config/ranger/rc.conf
 export VISUAL=micro && export PAGER=more 
 echo "Author's personal recommendation is to customize the appearance of Termux 🤩"
 echo "Settings by author: colors - 73, font - 11"
 echo 3
+rm -rf ~/storage/downloads/termux-style
 git clone "https://github.com/Zalexanninev15/termux-style-clone.git" ~/storage/downloads/termux-style && cd ~/storage/downloads/termux-style && bash ./install
 echo "Removing 'garbage' packages..."
 apt autoremove -y && apt autoclean
